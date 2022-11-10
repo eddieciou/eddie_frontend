a339239597a1f8d8e372490e8cfb2a75e8dfcc67
eddieciou <a589658gur@gmail.com>
2022-11-10 09:50:52 +0800

    實作tailwind RWD

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ProjectPage from './projects/ProjectPage';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import Game from './game/Game';
+import PracticePage from './practice/PracticePage';
 
 function App() {
   return (
@@ -32,10 +33,17 @@ function App() {
         >
           Game
         </NavLink>
+        <NavLink
+          to='/practice'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          Practice
+        </NavLink>
       </header>
       <div className='container'>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/practice' element={<PracticePage />} />
           <Route path='/game' element={<Game />} />
           <Route path='/projects' element={<ProjectsPage />} />
           <Route path='/projects/:id' element={<ProjectPage />} />
---
+++ src/practice/PracticePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RWDCardTwo from './RWDCardTwo';
+
+function PracticePage() {
+  return <RWDCardTwo />;
+}
+
+export default PracticePage;
